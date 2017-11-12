2b090557c734721c1e1f06909901674e91a1f035
gillilands19 <gillilands19@gmail.com>
2017-11-11 16:18:00 -0800

    added supports in functions/began header

--- Greenometry_Theme/functions.php
+++ Greenometry_Theme/functions.php
@@ -10,6 +10,56 @@
  * 
  **/
 
+
+/** 
+ * Add Meta Tags to head 
+ **/
+function add_meta_title_tags() {
+	 $grCharSet = get_bloginfo( 'charset' );
+	 $grDescription = get_bloginfo( 'description' );
+	 $grSiteTitle = get_bloginfo( 'name' );
+	 echo '<meta charset="' . $grCharSet . '" />';
+	 echo '<meta name="description" content="' . $grDescription . '" />';
+	 echo '<meta name="robots" content="noindex,nofollow" />';
+	 echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+	 //echo '<title>' . $grSiteTitle . '</title>';
+}
+add_action('wp_head', 'add_meta_title_tags');
+
+/** 
+ * Greenometry theme supports and registers
+**/
+function greenometry_theme_supports() {
+	
+	register_nav_menus( array( 
+		'main-menu'	=> __('Main'), 
+		'social' 	=> __('Social') 
+		) 
+	);
+
+	add_theme_support( 'html5', array(
+		'comment-form',
+		'search-form',
+		'comment-list',
+		'gallery',
+		'caption'
+		) 
+	);
+
+	add_theme_support( 'post-thumbnails' );
+
+	add_theme_support( 'title-tag' );
+
+	add_theme_support( 'custom-logo', array(
+		//add height
+		//add width
+		//add flex options
+		) 
+	);
+}
+add_action( 'after_setup_theme', 'greenometry_theme_supports' );
+
+
 /**
  * Add categories and tags to Page post-type 
  **/
@@ -81,3 +131,10 @@ function qa_title_placeholder( $title ) {
 }
 
 add_filter( 'enter_title_here', 'qa_title_placeholder' );
+
+/**
+ * TODO: 
+ * functions for loops that query post types for abstraction
+ * functions for multi loop pages. Look into post rewind or what function calls are needed for multiple loops
+ * function for meta tags
+ **/
---
+++ Greenometry_Theme/header.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Header for Greenometry Theme
+ * 
+ * @author Sean Gilliland, Melissa Wong, Analia Arredondo, Ronald Hamasaki
+ * @version 1.0 
+ * 
+ * 
+ * TODO: Add classes to elements
+ * finish nav menu args
+ * 
+ * 
+ **/
+?>
+
+
+<!DOCTYPE>
+<html>
+	<head>
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<header>
+			<figure>
+				<?php the_custom_logo(); ?>
+			</figure>
+			<?php wp_nav_menu( array(
+				'theme_location' 		=> 'main-menu',
+				'container'				=> 'nav',
+				'container_class'		=> '',
+				'items_wrap'			=> '<ul class="%2$s">%3$s</ul>', //TODO add classes to ul
+				)
+			); 
+				?>
+		</header>
+	
