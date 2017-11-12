5c10771c206c467525152fb022cec5426b80d5cf
gillilands19 <gillilands19@gmail.com>
2017-11-11 21:21:41 -0800

    started page template

--- Greenometry_Theme/page.php
+++ Greenometry_Theme/page.php
@@ -1,6 +1,29 @@
-<?php get_header(); ?>
+<?php  
+/**
+ * Page Template - Greenometry Theme
+ * 
+ * @author Sean Gilliland, Melissa Wong, Analia Arredondo, Ronald Hamasaki
+ * @version 1.0 
+ * 
+ * 
+ * TODO:
+ * 
+ **/
 
-		
-			
-			
-<?php get_footer(); ?>
+get_header();
+
+get_template_part( '/template-parts/page/content', 'page' );
+
+if ( is_page('About Us') ){
+	get_template_part('/template-parts/page/team', 'page' );
+	get_template_part('/template-parts/page/support', 'page');
+} else if ( is_page('Ask Greenometry') ) {
+	get_template_part('/template-parts/page/recent', 'qa');
+} else {
+	echo '<div></div>'; //Add some sort of divider
+}
+
+get_footer();
+
+
+?>
--- Greenometry_Theme/template-parts/page/content-page.php
+++ Greenometry_Theme/template-parts/page/content-page.php
@@ -1,3 +1,16 @@
+<?php  
+/**
+ * Content-Page Template Part - Greenometry Theme
+ * 
+ * @author Sean Gilliland, Melissa Wong, Analia Arredondo, Ronald Hamasaki
+ * @version 1.0 
+ * 
+ * 
+ * TODO: Add classes to elements 
+ * 
+ **/
+?>		
+		
 		<section>
 		
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
