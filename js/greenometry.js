f30c0c0c9d8d952612117395b54810d32a39cff3
gillilands19 <gillilands19@gmail.com>
2017-12-12 14:59:04 -0800

    styles single.php template, changes window width conditional for mobile nav

--- js/greenometry.js
+++ js/greenometry.js
@@ -5,13 +5,13 @@ jQuery( document ).ready( function( $ ) {
 		const navElem = [$('#open'), $('#close'), $('#navigation-container')];
 
 		$('#menu-icons').click(function() {
-			if($(window).width() < 900) {
+			if($(window).width() < 925) {
 				navElem.forEach(elem => elem.toggle());
 		}
 		});
 
 		$(window).on('resize', () => {
-			if($(window).width() > 900) {
+			if($(window).width() > 925) {
 				navElem.forEach(elem => elem.toggle());
 				$('#navigation-container').show();
 			} else {
--- single.php
+++ single.php
@@ -16,54 +16,30 @@
 
 get_header();
 ?>
-		single.php
-		<section class="posts-container">
+		<section class="single-post-container">
 			<?php if (have_posts()) : while (have_posts()) : the_post();?>
-
-			<?php 
-			
-
-			/**
-			 * This is exploratory for rewriting links when listing the categories, so that the link will redirect to a list of categories
-			 * that only includes one Post type. for example, I only want to see the Question and answers post type that has a category of technology.
-			 * 
-			 *  
-			 * The better option may be enabling a custom taxonomy for the Q&A post type. This way the custom taxonomy will be queried instead of the
-			 * Category Taxonomy which is attached to the Post object.
-			 * **/
-			
-			
-			// echo get_site_url() . '<br>';
-			// echo get_post_type() . '<br>';
-			// $category_list = get_the_category();
-			// var_dump($category_list);
-			// echo $category_list[0]->name;
-
-
-			// add_filter( 'term_link', function ($termlink) {
-			// 	$category_list = get_the_category();
-			// 	$termlink = get_site_url() . '/' . get_post_type() . '/' . $category_list[0]->slug;
-			// }, 10, 1 );
-			
-			?>
 
 			<?php 
 			if(get_post_type() == 'question_answer') {
 				$the_terms = get_terms( 'question_answer_category', array( 'hide_empty' => 0 ) );
 				echo get_term_link('technology', 'question_answer_category');
-				var_dump($the_terms);
 			}
 			
 
 			?>
 
-			<h1 class="posts-container__single-heading"><?php the_title(); ?></h1>
-			<div class="posts-container__single-details-container">
-				<small class="posts-container__single-details"><?php the_time( 'F j, Y' ); echo ' | '; the_category( ', ' ); echo ' | '; ?></small>
+			<h1 class="single-post-container__single-heading"><?php the_title(); ?></h1>
+			<div class="single-post-container__single-details-container">
+				<small class="single-post-container__single-details"><?php the_time( 'F j, Y' ); echo ' | '; the_category( ', ' ); echo ' | '; ?></small>
 				<?php //add social menu here ?>
 			</div>
+			<hr>
 
-			<article class="posts-container__single-content">
+			<figure class="single-post-container__post-thumbnail">
+					<?php the_post_thumbnail(); ?>
+				</figure>
+
+			<article class="single-post-container__single-content">
 				<?php $content = get_the_content();
 					the_content();
 					if(get_post_type() == 'question_answer') {
