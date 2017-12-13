ed3c149db64b8a6fea3da8fc750b49257c6e4be4
gillilands19 <gillilands19@gmail.com>
2017-12-12 19:31:36 -0800

    hardcodes support icons

--- template-parts/page/support-page.php
+++ template-parts/page/support-page.php
@@ -20,7 +20,7 @@
             <div id="support-container__options">
 				<div class="support-container__item-container1">
 					<figure class="support-container__icon">
-					<img id="donate" src="<?php the_field( 'image' ); ?>" alt="donate">
+					<img id="donate" src="http://www.seagill.com/greenometry/greenometry_test/wp-content/uploads/2017/11/donate1.png" alt="donate">
 					</figure>
 					<h3 class="support-container__item-heading">Donate</h3>
 					<p class="support-container__item-subtitle">Our projects and research <br> greatly appreciate your financial support.</p>
@@ -28,7 +28,7 @@
 	            
 				<div class="support-container__item-container2">
 					<figure class="support-container__icon">
-					<img id="donate" src="<?php the_sub_field( 'share_icon' ); ?>" alt="donate">
+					<img id="donate" src="http://www.seagill.com/greenometry/greenometry_test/wp-content/uploads/2017/11/share.png" alt="donate">
 					</figure>
 					<h3 class="support-container__item-heading">Share</h3>
 					<p class="support-container__item-subtitle">Help us spread the word and<br> share our page.</p>
@@ -36,7 +36,7 @@
 	            
 				<div class="support-container__item-container3">
 					<figure class="support-container__icon">
-					<img id="donate" src="<?php the_field( 'collaborate_icon' ); ?>" alt="donate">
+					<img id="donate" src="http://www.seagill.com/greenometry/greenometry_test/wp-content/uploads/2017/11/collab-1.png" alt="donate">
 					</figure>
 					<h3 class="support-container__item-heading">Collaborate</h3>
 					<p class="support-container__item-subtitle">Have ideas or contributions? <br>Get in touch!</p>
